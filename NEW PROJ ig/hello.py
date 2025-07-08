f5aab2e4f65450b735aaf4bfff922b2772c32d08
Sabelhur <89353531+Sabelhur@users.noreply.github.com>
2025-07-08 12:11:53 -0700

    figuring out syntaxes

---
+++ NEW PROJ ig/hello.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# msg = "Roll a dice!"
+# print(msg)
+
+# x = np.random.randint(1,9)
+
+# for i in range(x):
+#     print(i)
+
+
+# #review of functions :)
+# list=[]
+# def ok(num1,num2):
+#     return print(num1+num2)
+
+# ok(4,1)
+
+
+# class yap:
+#     def __init__(self, who, asked):
+#         self.who = who
+#         self.asked = asked
+    
+#     def ok(self, num1,num2):
+#         return print(num1+num2)
+
+
+# yapp = yap("ok","what")
+# yapp.ok(1,5)
+
+#Tokenization converting a sequence of characters into a sequence of tokens.
+#But first ima have to parse Its gonna have to use split, and then use a class in order to literallly token and give thingies a def soo lets practice that lol
+
+sentence = 'just a test \n again'
+print(sentence)
+x = sentence.split(" ")
+print(x)
